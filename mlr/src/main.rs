97f7b04aa620bc6ba40e945efecb46f4c044bb76
MainFoldFunc <gs.live.pub@gmail.com>
2025-03-19 17:34:21 +0100

    Main function

---
+++ mlr/src/main.rs
@@ -0,0 +1,8 @@
+mod matrix_functions;
+mod matrix_with_vector_functions;
+mod vectors_functions;
+fn main() {
+    //vectors_functions::test_vec();
+    //matrix_functions::test_matrix();
+    matrix_functions::test_vid();
+}
